10fb6b1c62d80f467793cf534dc021e72489e191
zhangrui <3993394595@qq.com>
2019-03-24 23:11:08 +0800

    增加爬虫代码

---
+++ crawler/config/custom.js
@@ -0,0 +1,9 @@
+module.exports = {
+    userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    viewPort:{width:1920,height:1080},
+    url:'http://www.szadoptpet.com/product',
+    initWaitFor:3000,
+    qiniu_domain:'http://qiniu.yunyangriji.com/',
+    server_port:'1337'
+}
+//http://pit6lysx0.bkt.clouddn.com/
